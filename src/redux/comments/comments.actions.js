d0a28d460cb78b8782fbdb2b25453d7ee28da1a1
ducan3007 <58422724+ducan3007@users.noreply.github.com>
2021-11-13 23:43:43 +0700

    gg

--- src/redux/comments/comments.actions.js
+++ src/redux/comments/comments.actions.js
@@ -50,7 +50,7 @@ export const addComment = (postId, formData) => async(dispatch) => {
         dispatch(getPost(postId));
         dispatch(getComments(postId));
     } catch (err) {
-        dispatch(setAlert(err.response.data.message, 'danger'));
+        //  dispatch(setAlert(err.response.data.message, 'danger'));
 
         dispatch({
             type: COMMENT_ERROR,
@@ -72,7 +72,7 @@ export const deleteComment = (PostId, CommentId) => async(dispatch) => {
         dispatch(setAlert(res.data.message, 'success'));
         dispatch(getPost(PostId));
     } catch (err) {
-        dispatch(setAlert((err.response.data.message || ''), 'danger'));
+        //  dispatch(setAlert((err.response.data.message || ''), 'danger'));
 
         dispatch({
             type: COMMENT_ERROR,
@@ -103,7 +103,7 @@ export const addAnswerComment = (PostId, answerId, formData) => async(dispatch) 
         dispatch(getAnswers(PostId));
 
     } catch (err) {
-        dispatch(setAlert((err.response.data.message || ''), 'danger'));
+        // dispatch(setAlert((err.response.data.message || ''), 'danger'));
 
         dispatch({
             type: COMMENT_ERROR,
@@ -141,7 +141,7 @@ export const deleteAnswerComment = (PostId, AnswerId, CommentId) => async(dispat
         dispatch(setAlert(res.data.message, 'success'));
         dispatch(getAnswers(PostId));
     } catch (err) {
-        dispatch(setAlert((err.response.data.message || ''), 'danger'));
+        // dispatch(setAlert((err.response.data.message || ''), 'danger'));
 
         dispatch({
             type: COMMENT_ERROR,
