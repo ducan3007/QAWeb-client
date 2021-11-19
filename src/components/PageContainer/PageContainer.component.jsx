cd861b3ca82ee266c3d74c4c4d83c9926f970e4b
anducnguyen <lordmasterking001@gmail.com>
2021-11-19 14:48:03 +0700

    add reponsive

--- src/components/PageContainer/PageContainer.component.jsx
+++ src/components/PageContainer/PageContainer.component.jsx
@@ -1,19 +1,23 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 
 import SideBar from '../SideBar/SideBar.component';
 import RightSideBar from '../RightSideBar/RightSideBar.component';
+
 import './PageContainer.styles.scss'
-const PageContainer = ({component: Component}) => {
+
+const PageContainer = ({ component: Component }) => {
   return class DefaultPageContainer extends React.Component {
     render() {
       return (
         <Fragment>
           <div className='page'>
             <SideBar />
-            <div id='content' style={{display:"flex",gap:"2%"}}>
-              <Component {...this.props} />
-              <RightSideBar />
+            <div id='content'>
+              <div id='content-inner'>
+                <Component {...this.props} />
+              </div>
             </div>
+            <RightSideBar />
           </div>
         </Fragment>
       );
