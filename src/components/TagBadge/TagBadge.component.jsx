6a1b8bcbd16449e08c46155471672a74644ffb40
anducnguyen <lordmasterking001@gmail.com>
2021-12-14 18:50:44 +0700

    d

--- src/components/TagBadge/TagBadge.component.jsx
+++ src/components/TagBadge/TagBadge.component.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import './TagBadge.styles.scss'
 
 const TagBadge = ({ tag_name, size, display, float, link, href }) => {
-
-  return (
+  
+  return tag_name === undefined ? ('') : (
     <div className="tags-badge" style={{ display, float }}>
       {href === true ? (
         <a className="m-tag" href={link ? link : `/tags/${encodeURIComponent(tag_name)}`}>
@@ -20,24 +20,4 @@ const TagBadge = ({ tag_name, size, display, float, link, href }) => {
 
 export default TagBadge;
 
-// import React from "react";
-// import htmlSubstring from "../../services/htmlSubstring";
-// import './TagBadge.styles.scss'
 
-// const TagBadge = ({ tag_name, size, display, float, link, href }) => {
-//   return (
-//     <div className="tags-badge" style={{ display, float }}>
-//       {href === true ? (
-//        <p className="tags-text"><a className="m-tag " href={link ? link : `/tags/${encodeURIComponent(tag_name)}`}>
-//           {htmlSubstring(decodeURIComponent(tag_name),10)}
-//         </a></p> 
-//       ) : (
-//         <p className="tags-text"><a className="m-tag" href={link ? link : `/tags/${encodeURIComponent(tag_name)}`}>
-//         {htmlSubstring(decodeURIComponent(tag_name),10)}
-//         </a></p> 
-//       )}
-//     </div>
-//   );
-// };
-
-// export default TagBadge;
